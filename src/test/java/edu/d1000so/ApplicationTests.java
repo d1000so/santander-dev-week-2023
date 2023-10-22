990e0b31e2d3c36a42ce1105e25346028dd2607f
edmilsonbraga <d1000so@unicamp.br>
2023-10-22 14:24:52 -0300

    Initial commit

---
+++ src/test/java/edu/d1000so/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.d1000so;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
